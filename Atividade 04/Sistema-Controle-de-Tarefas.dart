64802975de0c24600dffb26f3f96cbbfe7083b81
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-11-05 14:50:27 -0300

    finalizando exercicio04

--- Atividade 04/Sistema-Controle-de-Tarefas.dart
+++ Atividade 04/Sistema-Controle-de-Tarefas.dart
@@ -5,6 +5,7 @@ void main() {
   Map<String, dynamic> tarefa = {};
   int? opcao;
   bool verify = true;
+  int? tamanhoAntes;
 
   while (true) {
     print("-" * 30);
@@ -20,136 +21,258 @@ void main() {
     stdout.write('Digite uma das opções: ');
     String? input = stdin.readLineSync();
     opcao = (input != null && input.isNotEmpty) ? int.tryParse(input) : null;
-    switch (opcao) {
-      // Cria uma Tarefa
-      case 1:
-        while (true) {
-          stdout.write('\nDigite o Nome da Tarefa: ');
-          String? inputTitle = stdin.readLineSync();
-          inputTitle = inputTitle != null && inputTitle.isNotEmpty
-              ? inputTitle
-              : null;
+    if (opcao != null) {
+      switch (opcao) {
+        // Cria uma Tarefa
+        case 1:
+          while (true) {
+            stdout.write('\nDigite o Nome da Tarefa: ');
+            String? inputTitle = stdin.readLineSync();
+            inputTitle = inputTitle != null && inputTitle.isNotEmpty
+                ? inputTitle
+                : null;
 
-          if (inputTitle == null) {
-            print('Valor inválido!');
-            continue;
-          }
-
-          for (var mapa in tarefas) {
-            if (mapa['titulo'] == inputTitle) {
-              print('Já existe uma tarefa com esse nome.');
-              verify = false;
-              break;
-            } else {
-              verify = true;
+            if (inputTitle == null) {
+              print('Valor inválido!');
               continue;
             }
-          }
-          if (!verify) {
-            continue;
+
+            for (var mapa in tarefas) {
+              if (mapa['titulo'] == inputTitle) {
+                print('Já existe uma tarefa com esse nome.');
+                verify = false;
+                break;
+              } else {
+                verify = true;
+                continue;
+              }
+            }
+            if (!verify) {
+              continue;
+            }
+
+            stdout.write('\nDescreva a Tarefa: ');
+            String? inputDescribe = stdin.readLineSync();
+            inputDescribe = inputDescribe != null && inputDescribe.isNotEmpty
+                ? inputDescribe
+                : null;
+
+            if (inputDescribe == null) {
+              print('Valor inválido!');
+            }
+
+            tarefa = {};
+            tarefa.putIfAbsent('titulo', () => inputTitle);
+            tarefa.putIfAbsent('descricao', () => inputDescribe);
+            tarefa.putIfAbsent('concluida', () => 'pendente');
+            tarefas.add(tarefa);
+            break;
           }
 
-          stdout.write('\nDescreva a Tarefa: ');
-          String? inputDescribe = stdin.readLineSync();
-          inputDescribe = inputDescribe != null && inputDescribe.isNotEmpty
-              ? inputDescribe
+        // Visualiza Todas as Tarefas
+        case 2:
+          if (tarefas.isNotEmpty) {
+            for (var mapa in tarefas) {
+              print(
+                "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
+              );
+            }
+          } else {
+            print('Lista de Tarefas está Vazia');
+            break;
+          }
+
+        // Edita uma Tarefa
+        case 3:
+          if (tarefas.isNotEmpty) {
+            for (var mapa in tarefas) {
+              print(
+                "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
+              );
+            }
+          } else {
+            print('Lista de Tarefas está Vazia');
+            break;
+          }
+
+          stdout.write('Digite o nome da Tarefa que deseja Editar: ');
+          String? input = stdin.readLineSync();
+          input = input != null && input.isNotEmpty ? input : null;
+
+          if (input != null) {
+            for (var mapa in tarefas) {
+              if (mapa['titulo'] == input) {
+                continue;
+              } else {
+                verify = false;
+              }
+            }
+            if (!verify) {
+              print('Nome não encontrado!');
+              break;
+            }
+            print("-" * 30);
+            print(
+              '1. Nome\n'
+              '2. Descrição\n'
+              '3. Concluída\n',
+            );
+            print("-" * 30);
+
+            stdout.write(
+              'Digite o número correspondente ao que deseja editar: ',
+            );
+            String? inputEdit = stdin.readLineSync();
+            input = inputEdit != null && inputEdit.isNotEmpty
+                ? inputEdit
+                : null;
+            if (inputEdit != null) {
+              switch (inputEdit) {
+                case '1':
+                  stdout.write('Digite o novo Valor a ser Inserido: ');
+                  String? inputNovo = stdin.readLineSync();
+                  input = inputNovo != null && inputNovo.isNotEmpty
+                      ? inputNovo
+                      : null;
+
+                  if (inputNovo != null) {
+                    for (var mapa in tarefas) {
+                      if (mapa['titulo'] == inputNovo) {
+                        mapa.update('titulo', (valorNovo) => inputNovo);
+                      } else {
+                        verify = false;
+                      }
+                    }
+                    if (!verify) {
+                      print('Nome não encontrado!');
+                    }
+                  } else {
+                    print('Valor nulo Inválido!');
+                  }
+                case '2':
+                  stdout.write('Digite o novo Valor a ser Inserido: ');
+                  String? inputNovo = stdin.readLineSync();
+                  input = inputNovo != null && inputNovo.isNotEmpty
+                      ? inputNovo
+                      : null;
+
+                  if (inputNovo != null) {
+                    for (var mapa in tarefas) {
+                      if (mapa['descricao'] == inputNovo) {
+                        mapa.update('descricao', (valorNovo) => inputNovo);
+                      } else {
+                        verify = false;
+                      }
+                    }
+                    if (!verify) {
+                      print('Nome não encontrado!');
+                    }
+                  } else {
+                    print('Valor nulo Inválido!');
+                  }
+                case '3':
+                  stdout.write('Digite o novo Valor a ser Inserido: ');
+                  String? inputNovo = stdin.readLineSync();
+                  input = inputNovo != null && inputNovo.isNotEmpty
+                      ? inputNovo
+                      : null;
+
+                  if (inputNovo != null) {
+                    for (var mapa in tarefas) {
+                      if (mapa['concluida'] == inputNovo) {
+                        mapa.update('concluida', (valorNovo) => inputNovo);
+                      } else {
+                        verify = false;
+                      }
+                    }
+                    if (!verify) {
+                      print('Nome não encontrado!');
+                    }
+                  } else {
+                    print('Valor nulo Inválido!');
+                  }
+              }
+            }
+          } else {
+            print('Valor nulo Inválido!');
+          }
+
+        // Remove uma Tarefa
+        case 4:
+          if (tarefas.isNotEmpty) {
+            for (var mapa in tarefas) {
+              print(
+                "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
+              );
+            }
+          } else {
+            print('Lista de Tarefas está Vazia');
+            break;
+          }
+          stdout.write('Digite o nome da Tarefa que deseja Remover: ');
+          String? input = stdin.readLineSync();
+          input = input != null && input.isNotEmpty ? input : null;
+
+          if (input != null) {
+            tamanhoAntes = tarefas.length;
+            tarefas.removeWhere((mapa) => mapa["titulo"] == input);
+            bool removeu = tarefas.length < tamanhoAntes;
+
+            if (removeu) {
+              print('\nTarefa Removida!');
+            } else {
+              print('Tarefa não encontrada!');
+            }
+          } else {
+            print('Valor nulo Inválido!');
+          }
+
+        // Marca como Concluída uma Tarefa
+        case 5:
+          if (tarefas.isNotEmpty) {
+            for (var mapa in tarefas) {
+              print(
+                "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
+              );
+            }
+          } else {
+            print('Lista de Tarefas está Vazia');
+            break;
+          }
+
+          stdout.write(
+            'Digite o nome da Tarefa que deseja marcar como Concluída: ',
+          );
+          String? inputNome = stdin.readLineSync();
+          inputNome = inputNome != null && inputNome.isNotEmpty
+              ? inputNome
               : null;
 
-          if (inputDescribe == null) {
-            print('Valor inválido!');
+          if (inputNome != null) {
+            for (var mapa in tarefas) {
+              if (mapa['titulo'] == inputNome) {
+                mapa.update('concluida', (valor) => 'concluída');
+                print('\nTarefa Concluída!');
+              } else {
+                verify = false;
+              }
+            }
+            if (!verify) {
+              print('Nome não encontrado!');
+            }
+          } else {
+            print('Valor nulo Inválido!');
           }
 
-          tarefa = {};
-          tarefa.putIfAbsent('titulo', () => inputTitle);
-          tarefa.putIfAbsent('descricao', () => inputDescribe);
-          tarefa.putIfAbsent('concluida', () => 'pendente');
-          tarefas.add(tarefa);
-          break;
-        }
+        // Termina o código
+        case 6:
+          return;
 
-      // Visualiza Todas as Tarefas
-      case 2:
-        if (tarefas.isNotEmpty) {
-          for (var mapa in tarefas) {
-            print(
-              "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
-            );
-          }
-        } else {
-          print('Lista de Tarefas está Vazia');
-          break;
-        }
-
-      // Edita uma Tarefa
-      case 3:
-        if (tarefas.isNotEmpty) {
-          for (var mapa in tarefas) {
-            print(
-              "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
-            );
-          }
-        } else {
-          print('Lista de Tarefas está Vazia');
-          break;
-        }
-
-        stdout.write('Digite o nome da tarefa que deseja editar: ');
-        String? input = stdin.readLineSync();
-        input = input != null && input.isNotEmpty ? input : null;
-
-        if (input != null) {}
-
-      // Remove uma Tarefa
-      case 4:
-        if (tarefas.isNotEmpty) {
-          for (var mapa in tarefas) {
-            print(
-              "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
-            );
-          }
-        } else {
-          print('Lista de Tarefas está Vazia');
-          break;
-        }
-        stdout.write('Digite o nome da tarefa que deseja remover: ');
-        String? input = stdin.readLineSync();
-        input = input != null && input.isNotEmpty ? input : null;
-
-        if (input != null) {
-          tarefas.removeWhere((mapa) => mapa["titulo"] == input);
-          print('\nTarefa Removida!');
-        }
-
-      // Marca como Concluída uma Tarefa
-      case 5:
-        if (tarefas.isNotEmpty) {
-          for (var mapa in tarefas) {
-            print(
-              "Nome: ${mapa['titulo']} - Descrição: ${mapa['descricao']} - Concluída: ${mapa['concluida']}",
-            );
-          }
-        } else {
-          print('Lista de Tarefas está Vazia');
-          break;
-        }
-
-        stdout.write(
-          'Digite o nome da tarefa que deseja marcar como Concluída: ',
-        );
-        String? inputNome = stdin.readLineSync();
-        inputNome = inputNome != null && inputNome.isNotEmpty
-            ? inputNome
-            : null;
-
-        if (inputNome != null) {
-          for (var mapa in tarefas) {
-            if (mapa == inputNome) {}
-          }
-        }
-
-      // Termina o código
-      case 6:
-        return;
+        default:
+          print('Valor inválido!');
+      }
+    } else {
+      print('Valor inválido!');
     }
   }
 }
