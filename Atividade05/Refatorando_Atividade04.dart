6f53132a480a553c2c256eeea77d39129f7c7b0d
Davi-Melo-Ferreira <davidemeloferreira13@gmail.com>
2025-11-12 16:23:54 -0300

    corrigindo refatoração

--- Atividade05/Refatorando_Atividade04.dart
+++ Atividade05/Refatorando_Atividade04.dart
@@ -1,6 +1,15 @@
 import 'dart:io';
 
-listarTarefas(tarefas, verify) {
+entrarDados(texto) {
+  stdout.write('$texto: ');
+  String? inputEntrada = (stdin.readLineSync() ?? '').trim();
+  if (inputEntrada == '') {
+    print('Valor inválido!');
+  }
+  return inputEntrada;
+}
+
+listarTarefas(tarefas) {
   if (tarefas.isNotEmpty) {
     print("\n--- Lista de Tarefas ---");
     for (var mapa in tarefas) {
@@ -10,17 +19,98 @@ listarTarefas(tarefas, verify) {
     }
   } else {
     print('Lista de Tarefas está Vazia');
-    return verify = false;
+    return '';
   }
 }
 
-entrarDados(texto) {
-  stdout.write('$texto: ');
-  String? inputEntrada = (stdin.readLineSync() ?? '').trim();
-  if (inputEntrada == '') {
-    print('Valor inválido!');
+// função criar tarefa
+void criarTarefa(tarefas) {
+  while (true) {
+    var inputTitle = entrarDados('Digite o nome da Tarefa que deseja');
+
+    if (inputTitle == '') continue;
+
+    if (tarefas.any((t) => t['titulo'] == inputTitle)) {
+      print("Já existe uma tarefa com esse título!\n");
+      continue;
+    }
+
+    var inputDescribe = entrarDados('Digite a Descrição da Tarefa');
+    if (inputDescribe == '') continue;
+
+    tarefas.add({
+      'titulo': inputTitle,
+      'descricao': inputDescribe,
+      'concluida': 'pendente',
+    });
+    break;
   }
-  return inputEntrada;
+}
+
+// função editar tarefa
+editarTarefa(tarefas, verify) {
+  var verificar = listarTarefas(tarefas);
+  if (verificar == false) return '';
+
+  var input = entrarDados('Digite o nome da Tarefa que deseja Editar');
+  if (input == '') return '';
+
+  try {
+    tarefas.firstWhere((t) => t['titulo'] == input);
+  } catch (StateError) {
+    print('Nome não encontrado!');
+    return '';
+  }
+
+  print("-" * 30);
+  print(
+    '1. Nome\n'
+    '2. Descrição\n',
+  );
+  print("-" * 30);
+
+  var inputEdit = entrarDados(
+    'Digite o número que corresponde à opção desejada',
+  );
+  if (inputEdit == '') return '';
+  switch (inputEdit) {
+    case '1':
+      entradaEditar('titulo', verify, tarefas, input);
+    case '2':
+      entradaEditar('descricao', verify, tarefas, input);
+    default:
+      print('Valor inválido!');
+  }
+}
+
+// função remover tarefa
+removerTarefa(tarefas, tamanhoAntes) {
+  var verificar = listarTarefas(tarefas);
+  if (verificar == false) return '';
+  var input = entrarDados('Digite o nome da Tarefa que deseja Remover');
+  if (input == '') return 'continue';
+
+  tamanhoAntes = tarefas.length;
+  tarefas.removeWhere((mapa) => mapa["titulo"] == input);
+  bool removeu = tarefas.length < tamanhoAntes;
+
+  if (removeu) {
+    print('\nTarefa Removida!');
+  } else {
+    print('Tarefa não encontrada!');
+  }
+}
+
+// função concluir tarefa
+concluirTarefa(tarefas, verify){
+  var verificar = listarTarefas(tarefas);
+  if (verificar == false) return;
+
+  var inputNome = entrarDados(
+    'Digite o nome da Tarefa que deseja Concluir',
+  );
+
+  entradaEditar('concluida', verify, tarefas, inputNome);
 }
 
 void entradaEditar(opcao, verify, tarefas, input) {
@@ -41,7 +131,7 @@ void entradaEditar(opcao, verify, tarefas, input) {
       }
     }
     if (!verify) print('Nome não encontrado!');
-  } else 
+  } else
     print('Valor inválido!');
 }
 
@@ -66,94 +156,23 @@ void main() {
     switch (opcao) {
       // Cria uma Tarefa
       case '1':
-        while (true) {
-          var inputTitle = entrarDados('Digite o nome da Tarefa que deseja');
-
-          if (inputTitle == '') continue;
-
-          if (tarefas.any((t) => t['titulo'] == inputTitle)) {
-            print("Já existe uma tarefa com esse título!");
-            continue;
-          }
-
-          var inputDescribe = entrarDados('Digite a Descrição da Tarefa');
-          if (inputDescribe == '') continue;
-
-          tarefas.add({
-            'titulo': inputTitle,
-            'descricao': inputDescribe,
-            'concluida': 'pendente',
-          });
-          break;
-        }
+        criarTarefa(tarefas);
 
       // Visualiza Todas as Tarefas
       case '2':
-        var verificar = listarTarefas(tarefas, verify);
-        if (verificar == false) break;
+        listarTarefas(tarefas);
 
       // Edita uma Tarefa
       case '3':
-        var verificar = listarTarefas(tarefas, verify);
-        if (verificar == false) break;
-
-        var input = entrarDados('Digite o nome da Tarefa que deseja Editar');
-        if (input == '') break;
-
-        try{
-          tarefas.firstWhere((t) => t['titulo'] == input);
-        } catch(StateError){
-          print('Nome não encontrado!');
-          break;
-        }
-
-        print("-" * 30);
-        print(
-          '1. Nome\n'
-          '2. Descrição\n',
-        );
-        print("-" * 30);
-
-        var inputEdit = entrarDados(
-          'Digite o número que corresponde à opção desejada',
-        );
-        if (inputEdit == '') break;
-        switch (inputEdit) {
-          case '1':
-            entradaEditar('titulo', verify, tarefas, input);
-          case '2':
-            entradaEditar('descricao', verify, tarefas, input);
-          default:
-            print('Valor inválido!');
-        }
+        editarTarefa(tarefas, verify);
 
       // Remove uma Tarefa
       case '4':
-        var verificar = listarTarefas(tarefas, verify);
-        if (verificar == false) break;
-        var input = entrarDados('Digite o nome da Tarefa que deseja Remover');
-        if (input == '') continue;
-
-        tamanhoAntes = tarefas.length;
-        tarefas.removeWhere((mapa) => mapa["titulo"] == input);
-        bool removeu = tarefas.length < tamanhoAntes;
-
-        if (removeu) {
-          print('\nTarefa Removida!');
-        } else {
-          print('Tarefa não encontrada!');
-        }
+        removerTarefa(tarefas, tamanhoAntes);
 
       // Marca como Concluída uma Tarefa
       case '5':
-        var verificar = listarTarefas(tarefas, verify);
-        if (verificar == false) break;
-
-        var inputNome = entrarDados(
-          'Digite o nome da Tarefa que deseja Concluir',
-        );
-
-        entradaEditar('concluida', verify, tarefas, inputNome);
+        
 
       // Termina o código
       case '6':
